01913dd1956787bb1621e371cd6c1fc093910988
DhLYa <dhyeyyajnik@gmail.com>
2026-07-20 16:24:17 +0100

    ingest.py added function

--- rag/ingest.py
+++ rag/ingest.py
@@ -1,2 +1,10 @@
-def load_docuuments(docs_dir: Path) -> list[Document]:
-    return
+from collections import defaultdict
+from pathlib import Path
+
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_core.documents import Document
+from .config import DOCS_DIR
+
+def load_documents(docs_dir: Path = DOCS_DIR) -> list[Document]:
+    loader = PyPDFDirectoryLoader(str(docs_dir))
+    return loader.load()
