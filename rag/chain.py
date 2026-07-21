060a9a3d0c2d3805c4e7bb753889fa5746ea8adf
DhLYa <dhyeyyajnik@gmail.com>
2026-07-21 22:14:01 +0100

    added functionality to chain.py

--- rag/chain.py
+++ rag/chain.py
@@ -1,14 +1,36 @@
-def get_llm():
-    return
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.documents import Document
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnablePassthrough
+from langchain_google_genai import ChatGoogleGenerativeAI
+ 
+from .config import LLM_MODEL
 
-def format_docs(docts: list[Document]) -> str:
-    return
+
+def get_llm():
+    llm = ChatGoogleGenerativeAI(model=LLM_MODEL, temperature=0)
+    return llm
+
+def format_docs(docs: list[Document]) -> str:
+    return "\n\n".join(doc.page_content for doc in docs)
 
 def build_prompt() -> ChatPromptTemplate:
-    return
+    return ChatPromptTemplate.from_template("""
+    Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, say that you don't know.
+    Context: {context}
+    Question: {question}
+    """)
 
 def build_chain(retriever) -> Runnable:
-    return
+    chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | build_prompt()
+    | get_llm()
+    | StrOutputParser()
+    )
+    return chain
 
 def ask(chain, question: str) -> str:
-    return
+    return chain.invoke(question)
